59151e31ce7799c44afbfb2c8eae07895e16900a
pikson <goverperson@gmail.com>
2022-08-09 23:00:04 +0800

    用webRequest代替devtools

--- background.js
+++ background.js
@@ -7,7 +7,20 @@ chrome.contextMenus.create(
     }
 );
 console.log("测试背景")
-chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+//使用webRequest来拦截请求
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        //将获取到的url传递给content_script
+        chrome.tabs.query({active:true},function(tabs){
+            if (details.url.indexOf(".m3u8")!=-1){
+                chrome.tabs.sendMessage(tabs[0].id,{type:'sendData',data:details.url})
+            }
+            
+        })
+    },
+    {urls:["<all_urls>"]},
+)
+/* chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
     if (message.name === "requestinfo") {
         if (message.content) {
             let data = message.content
@@ -19,4 +32,4 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
            
         }
     }
-});
+}); */
